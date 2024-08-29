1816b81005645d5451772247ad449746cc5e8d64
Jimena1011 <jivargas@estudiantec.cr>
2024-08-28 18:18:13 -0600

    Module_decoder
    Se agrego la parte del 4.3

--- Tarea 1/src/design/module_decoder.sv
+++ Tarea 1/src/design/module_decoder.sv
@@ -2,11 +2,14 @@
 // Modulo de conexion
 
 module principal (
+
    input logic [3:0] GrayCode,
+   input logic btn_in;
 
    output logic led3, led2, led1, led0
-   //Anadir mas salidas, como la del 7-segmentos
-
+   output logic a, b, c, d, e, f   //Anadir salidas, como la del 7-segmentos
+   output logic ad, bd, cd, dd, ed, fd 
+  
 );
 
 logic [3:0]bit;
@@ -72,52 +75,44 @@ module binary_leds (
 
 
 
-
-
 //4.3
-
-module binary_to_hexadecimal (
-   input logic ,
-   ouput logic 
-);
-
-endmodule
-
-module hexadecimal_to_sevensegment (
-
-);
-
-
-endmodule
-
-
-
-//Revisar Pong Chu FPGA, pag.68  , para hex_to_seven_seg
-module binary_to_sevenseg (
-   input logic bit3, bit2, bit1, bit0,
+// Para las unidades de 7 setmentos 
+module decodificador_siente (
+   input logic led0, led1, led2, led3,
    output logic a, b, c, d, e, f, g
 );
 
-gray_to_binary 7segment (
-   .b3(bit3),
-   .b2(bit2),
-   .b1(bit1),
-   .b0(bit0)
-);
+   assign a = (~led1 & ~led3) | (led1 & led3) | (led2) | (led0);
+   assign b = (~led0 & ~led1) | (~led3) | (led0 & led1);
+   assign c = (~led0) | (led3) | (led1);
+   assign d = (~led1 & ~led3) | (~led0 & led1 & led3) | (led0 & ~led1) | (led0 & ~led3);
+   assign e = (~led1 & ~led3) | (led0 & ~led1);
+   assign f = (~led0 & ~led1) | (~led0 & led3) | (led2) | (~led1 & led3);
+   assign g = (led2) | (~led0 & led3) | (led0 & ~led3) | (~led1 & led3);
 
 endmodule
-
-
-
 
 module control_button(
-   input logic ,
-   output logic ,
-
+   input logic btn_in;
+   ouput logic ad, bd, cd, dd, ed, fd, gd
 );
 endmodule
+// Módulo del decodificador para 7 segmentos (decenas)
+module decodificador_decenas (
+   input logic btn_in,
+   output logic ad, bd, cd, dd, ed, fd, gd
+);
 
+   // Cuando el botón está presionado, se muestra el número 1
+   assign ad = btn_in ? 1'b0 : 1'b1;
+   assign bd = btn_in ? 1'b1 : 1'b0;
+   assign cd = btn_in ? 1'b1 : 1'b0;
+   assign dd = btn_in ? 1'b0 : 1'b1;
+   assign ed = btn_in ? 1'b0 : 1'b1;
+   assign fd = btn_in ? 1'b0 : 1'b1;
+   assign gd = btn_in ? 1'b0 : 1'b1;
 
+endmodule
 
 
 
